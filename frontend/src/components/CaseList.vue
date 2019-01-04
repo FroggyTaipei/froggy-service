58cd0f27dd1d1c62561795998a50222ab841b5f7
ankycheng <ankycheng@gmail.com>
2019-01-04 18:42:17 +0800

    add frontend package and vue components

---
+++ frontend/src/components/CaseList.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+.container-fluid.section
+  h1 Cases
+
+</template>
+
+<script>
+export default {
+  name: 'CaseList'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+nav.navbar.navbar-expand-lg.navbar-light.fixed-top.bg-custom.scroll(:class="{scrolled:scrolled}")
+  .container
+    a.navbar-brand(href='#') LOGO 在這邊
+    button.navbar-toggler(type='button', data-toggle='collapse', data-target='#navbarSupportedContent', aria-controls='navbarSupportedContent', aria-expanded='false', aria-label='Toggle navigation')
+      span.navbar-toggler-icon
+    #navbarSupportedContent.collapse.navbar-collapse
+      ul.navbar-nav.ml-auto
+        li.nav-item
+          a.nav-link(href='#dialogue') 我要找呱吉 
+        li.nav-item
+          a.nav-link(href='#cases') 呱吉做什麼
+        li.nav-item
+          a.nav-link(href='#about') 關於公開選服
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  data: function(){
+    return {
+      scrolled: false
+    }
+  },
+  methods:{
+    checkScroll(){
+      var startY = window.innerHeight/2;
+      this.scrolled = window.scrollY > startY;
+    }
+  },
+  mounted(){
+    window.addEventListener('scroll', this.checkScroll)
+  },
+  destroyed(){
+    window.removeEventListener('scroll', this.checkScroll)
+  }
+}
+</script>
+
+<style lang="sass">
+
+</style>
