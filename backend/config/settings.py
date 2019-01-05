a09e397f8c8146b6f498f258c4d32c6d1d15941a
travishen.tw <travishen.tw@gmail.com>
2019-01-06 03:58:26 +0800

    Add docker-stack-prod for production
    * Add deployment from Travis CI to docker hub

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'NAME': env.str('POSTGRES_DB'),
         'USER': env.str('POSTGRES_USER'),
         'PASSWORD': env.str('POSTGRES_PASSWORD'),
-        'HOST': 'postgres',
+        'HOST': env.str('POSTGRES_HOST'),
         'PORT': 5432,
     },
 }
