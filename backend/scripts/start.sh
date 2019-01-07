a9ebf4c66fac84830b3f76b5c3b9ca4b50a0f034
travishen.tw <travishen.tw@gmail.com>
2019-01-07 16:48:16 +0800

    Update travis ci deployment

--- backend/scripts/start.sh
+++ backend/scripts/start.sh
@@ -6,7 +6,6 @@ set -o nounset
 set -o xtrace
 
 python manage.py migrate
-echo "from apps.users.models import User; User.objects.create_superuser('admin@example.com', 'pass')" | python manage.py shell
 python manage.py compilemessages
 python manage.py loaddata region status type
 python manage.py loaddata case.test.yaml arrange.test.yaml
--- docker_deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
-    docker push froggytaipei/froggy-service-nginx;
-    docker push froggytaipei/froggy-service-api;
-fi
