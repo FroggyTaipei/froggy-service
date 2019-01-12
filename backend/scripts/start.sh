42bed8f2f0d264f1f7cdaa1d9194877617ad3715
travishen.tw <travishen.tw@gmail.com>
2019-01-12 18:13:25 +0800

    Update sengrid templates

--- backend/scripts/start.sh
+++ backend/scripts/start.sh
@@ -8,7 +8,7 @@ set -o xtrace
 python manage.py migrate
 python manage.py compilemessages
 python manage.py loaddata region type
-python manage.py loaddata case.test.yaml arrange.test.yaml
+python manage.py loaddata case.test.yaml arrange.test.yaml sendgrid-template.test.yaml
 echo "from apps.users.tests.initial import create_superuser" | python manage.py shell
 python manage.py collectstatic --noinput --verbosity 0
 python manage.py runserver_plus 0.0.0.0:8000
