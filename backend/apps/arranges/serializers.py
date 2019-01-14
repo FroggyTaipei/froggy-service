6f963c7e208cf7f66fea97a95b14c8054623c056
travishen.tw <travishen.tw@gmail.com>
2019-01-14 20:25:39 +0800

    Update cases admin
    * Set nginx server_name
    * Update test data

--- backend/apps/arranges/serializers.py
+++ backend/apps/arranges/serializers.py
@@ -3,6 +3,10 @@ from .models import Arrange
 
 
 class ArrangeSerializer(serializers.ModelSerializer):
+    arrange_time = serializers.SerializerMethodField()
+
+    def get_arrange_time(self, obj):
+        return obj.format_arrange_time()
 
     class Meta:
         model = Arrange
