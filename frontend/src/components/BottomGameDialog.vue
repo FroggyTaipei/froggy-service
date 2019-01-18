e07e1ae0fd7722fc534953e9c0d092b118a6ab65
ankycheng <ankycheng@gmail.com>
2019-01-18 15:30:34 +0800

    fix frontend img url and edit layout

---
+++ frontend/src/components/BottomGameDialog.vue
@@ -0,0 +1,59 @@
+<template lang="pug">
+  el-row.row-dialog(type="flex" align="middle" justify="center" style="height:100%")
+    .bottom-dialog-wrapper
+      .bottom-dialog-left
+        h2(v-for="t in title") {{ t }}
+      .bottom-dialog-right
+        span.bottom-dialog-options
+          a(href="#firstPage") 我要服務
+        span.bottom-dialog-options 
+          a(href="#secondPage") 呱吉做什麼
+        span.bottom-dialog-options
+          a(href="#thirdPage") 關於魔鏡號
+        span.bottom-dialog-options -
+</template>
+
+<script>
+export default {
+  name: 'BottomGameDialog',
+  props: ['title']
+}
+</script>
+
+<style lang="sass" scoped>
+
+.bottom-dialog-wrapper
+  flex: 1
+  position: absolute
+  display: flex
+  flex-direction: row
+  // bottom: 0
+  padding: 5px
+  background-color: rgba(0,0,0,1)
+  width: 100%
+  height: 100%
+  .bottom-dialog-left
+    background-color: white
+    border: solid 5px gray
+    border-radius: 20px
+    margin-right: 5px
+    flex: 3
+    h2
+      padding: 0px 10px 0px 10px
+  .bottom-dialog-right
+    background-color: white
+    border: solid 5px gray
+    border-radius: 20px
+    flex: 2
+    display: flex
+    flex-direction: row
+    flex-wrap: wrap
+    align-items: center
+    justify-content: center
+    .bottom-dialog-options
+      flex-basis: calc(50%)
+      justify-content: center
+      flex-direction: column
+      text-align: center
+      font-size: 1.2em
+</style>
--- frontend/src/components/CaseList.vue
+++ frontend/src/components/CaseList.vue
@@ -15,7 +15,7 @@ el-container.page2
                   span {{ props.row.type }}
                 el-form-item(label='處理進度')
                   span {{ props.row.state }}
-          el-table-column(prop='number', label='案件編號', width='100', sortable)
+          el-table-column(prop='number', label='案件編號', width='140', sortable)
             //- template(slot-scope='props')
               span {{ props.row.location }}
               el-popover(trigger='hover', placement='top')
@@ -24,7 +24,7 @@ el-container.page2
                 .name-wrapper(slot='reference')
                   el-tag(size='medium') 
           el-table-column(prop='create_time', label='陳情日期')
-          el-table-column(prop='type', label='案件類別', width='100')
+          el-table-column(prop='type', label='案件類別', width='120')
           el-table-column(prop='title', label='案件主旨', width='180')
           el-table-column(prop='state', label='處理進度')
       //- el-row(type="flex" align="middle" justify="center")
@@ -35,28 +35,20 @@ el-container.page2
           span.dialog-footer(slot='footer')
             el-button(@click='dialogVisible = false') 取 消
             el-button(type='primary', @click='dialogVisible = false') 确 定
-    el-row.row-dialog(type="flex" align="middle" justify="center")
-      .bottom-dialog-wrapper
-        .bottom-dialog-left
-          h1 呱吉做什麼.......
-        .bottom-dialog-right
-          span.bottom-dialog-options 我要服務
-          span.bottom-dialog-options 呱吉做什麼
-          span.bottom-dialog-options 關於魔鏡號
-          span.bottom-dialog-options -
-
-
+    BottomGameDialog(:title="title")
 </template>
 
 <script>
 import Modal from './Modal.vue'
 import Navbar from './Navbar.vue'
+import BottomGameDialog from './BottomGameDialog.vue'
 
 export default {
   name: 'CaseList',
-  components: {Modal, Navbar},
+  components: {Modal, Navbar, BottomGameDialog},
   data(){
     return {
+      title:['呱吉做什麼.......'],
       tableData: [],
       clickdata: null,
       showModal: false,
@@ -86,14 +78,7 @@ export default {
   background-image: url('https://s3-ap-southeast-1.amazonaws.com/o-r-z/froggy-service/gradient_background.png')
   background-position: center
   overflow: hidden
-.el-menu--horizontal
-  border: none
-.el-menu--horizontal>.el-menu-item.is-active
-  border-bottom-color: transparent
-  border-bottom: none
-.el-menu-item
-  a
-    text-decoration: none
+
 .el-main
   display: flex
   flex-shrink: 0
@@ -111,41 +96,5 @@ export default {
   flex: 7
 .row-dialog
   flex: 1
-
-.bottom-dialog-wrapper
-  position: absolute
-  display: flex
-  flex-direction: row
-  // bottom: 0
-  padding: 5px
-  background-color: rgba(0,0,0,1)
-  width: 100%
-  height: 100%
-  .bottom-dialog-left
-    background-color: white
-    border: solid 5px gray
-    border-radius: 20px
-    margin-right: 5px
-    flex: 3
-    h1
-      padding: 20px
-      font-size: 40px
-  .bottom-dialog-right
-    background-color: white
-    border: solid 5px gray
-    border-radius: 20px
-    flex: 2
-    display: flex
-    flex-direction: row
-    flex-wrap: wrap
-    align-items: center
-    justify-content: center
-    .bottom-dialog-options
-      flex-basis: calc(50%)
-      justify-content: center
-      flex-direction: column
-      text-align: center
-      font-size: 1.2em
-
 
 </style>
