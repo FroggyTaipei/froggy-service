f966a74ec9c26263c04c6695d19a77922cbefc59
travishen.tw <travishen.tw@gmail.com>
2019-01-22 23:00:22 +0800

    Add google storage credential file
    * Remove ckeditor uploader
    * Remove S3 settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -35,7 +35,6 @@ THIRD_PARTY_APPS = [
     'rest_framework',
     'django_extensions',
     'ckeditor',
-    'ckeditor_uploader',
     'storages',
     'suit_ckeditor',
     'fsm_admin',
@@ -356,19 +355,6 @@ FIXTURE_DIRS = (
 
 # Cloud Storage
 # ------------------------------------------------------------------------------
-USE_AWS_S3 = env.bool('USE_AWS_S3', default=False)
-if USE_AWS_S3:
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    AWS_ACCESS_KEY_ID = env.str('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = env.str('AWS_SECRET_ACCESS_KEY')
-    AWS_STORAGE_BUCKET_NAME = env.str('AWS_STORAGE_BUCKET_NAME')
-    AWS_S3_OBJECT_PARAMETERS = {
-        'CacheControl': 'max-age=86400',
-    }
-    # For ckeditor uploader only, custom private backend on other usage
-    AWS_DEFAULT_ACL = 'public-read'
-    AWS_QUERYSTRING_AUTH = False
-
 USE_GCS = env.bool('USE_GCS', default=False)
 if USE_GCS:
     DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
@@ -380,12 +366,6 @@ if USE_GCS:
         f"{ROOT_DIR}/{env.str('GS_CREDENTIALS')}",
     )
 
-# CKEDITOR
-# ------------------------------------------------------------------------------
-CKEDITOR_UPLOAD_PATH = 'public-uploads/'
-CKEDITOR_IMAGE_BACKEND = 'pillow'
-CKEDITOR_RESTRICT_BY_DATE = False
-CKEDITOR_FILENAME_GENERATOR = 'apps.files.utils.generate_filename'
 
 # DJANGO SUIT
 # ------------------------------------------------------------------------------
--- before_install.sh
+++ before_install.sh
@@ -3,6 +3,8 @@
 if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ];
 then
   openssl aes-256-cbc -K $encrypted_1fa28bd21641_key -iv $encrypted_1fa28bd21641_iv -in .env.enc -out .env -d
+  openssl aes-256-cbc -K $encrypted_1fa28bd21641_key -iv $encrypted_1fa28bd21641_iv -in gs_credential.json.enc -out backend/gs_credential.json -d
 else
   sudo cp .env.example .env
+  sudo touch backend/gs_credential.json
 fi
