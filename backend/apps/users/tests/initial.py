73506898dbe8c54bea6a119cb18459a37a670b9a
travishen.tw <travishen.tw@gmail.com>
2019-01-24 14:08:48 +0800

    Add test jwt token
    * docker-compose up will generate user token automatically
    * set VUE_APP_USER_JWT in your local .env to use it

--- backend/apps/users/tests/initial.py
+++ backend/apps/users/tests/initial.py
@@ -1,13 +1,39 @@
+import logging
+from datetime import datetime, timedelta
+
+from apps.users.utils import jwt_encode_handler
 from apps.users.models import User
+
+
+logger = logging.getLogger('raven')
 
 
 def create_superuser():
     """Initial fake superuser only for testing. Use in docker-compose."""
-    email = 'test@mail.com'
+    email = 'test@test.test'
     password = '123456'
+    mobile = '0912345678'
     if not User.objects.count():
-        User.objects.create_user(email=email, password=password, full_name='測試用超級使用者',
-                                 is_staff=True, is_superuser=True)
+        user = User.objects.create_user(email=email, password=password,
+                                        full_name='Test Superuser', mobile=mobile,
+                                        is_staff=True, is_superuser=True)
+        payload = {
+            'id': user.pk,
+            'exp': datetime(year=2019, month=1, day=1) + timedelta(days=3),
+        }
+        token = jwt_encode_handler(payload)
+        logger.info(f"""
+
+Your initial superuser has been set for testing, use this user to sign in Django admin:
+
+    Email: {email}
+    Password: {password}
+
+Use this token for jwt testing, expired in 3 days:
+
+    Authorization: JWT {token}
+
+""")
 
 
 create_superuser()
--- backend/scripts/start.sh
+++ backend/scripts/start.sh
@@ -7,8 +7,10 @@ set -o xtrace
 
 python manage.py migrate
 python manage.py compilemessages
+python manage.py collectstatic --noinput --verbosity 0
+
 python manage.py loaddata region type sendgrid-template.yaml
 python manage.py loaddata cases.test.yaml arranges.test.yaml
 echo "from apps.users.tests import initial" | python manage.py shell
-python manage.py collectstatic --noinput --verbosity 0
+
 python manage.py runserver_plus 0.0.0.0:8000
