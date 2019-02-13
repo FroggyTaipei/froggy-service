0ad13d078e09c282809463b243f7250428606617
travishen.tw <travishen.tw@gmail.com>
2019-02-13 19:56:48 +0800

    Dispatch env, update encrypts

--- before_install.sh
+++ before_install.sh
@@ -4,6 +4,7 @@ if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ];
 then
   openssl aes-256-cbc -K $encrypted_1fa28bd21641_key -iv $encrypted_1fa28bd21641_iv -in secrets.tar.enc -out secrets.tar -d
   tar xvf secrets.tar
+  sudo cp .env.travis .env
   sudo cp gs_credential.json backend/gs_credential.json
 else
   sudo cp .env.example .env
