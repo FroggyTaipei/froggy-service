3ae9d0450d4a9b7be4ba6b23e357a28bae477b4b
travishen.tw <travishen.tw@gmail.com>
2019-02-14 22:17:45 +0800

    Add stage deploy

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -2,19 +2,40 @@
 
 set -e
 
+RELEASE_NGINX=gcr.io/${GOOGLE_PROJECT_ID}/${NGINX_IMAGE}
+RELEASE_API=gcr.io/${GOOGLE_PROJECT_ID}/${API_IMAGE}
+STAGE_NGINX=gcr.io/${GOOGLE_PROJECT_ID}/${STAGE_NGINX_IMAGE}
+STAGE_API=gcr.io/${GOOGLE_PROJECT_ID}/${STAGE_API_IMAGE}
+PUBLIC_NGINX=${DOCKER_ORG}/${NGINX_IMAGE}
+PUBLIC_API=${DOCKER_ORG}/${API_IMAGE}
+
 if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 
     # Use Credential file to access google cloud
     gcloud auth activate-service-account --key-file gs_credential.json
     gcloud auth configure-docker
 
-    # Build image and push to google container registry
-    docker image build -t gcr.io/${GOOGLE_PROJECT_ID}/${NGINX_IMAGE}:$TRAVIS_COMMIT -f nginx/k8s.Dockerfile .;
-    docker image build -t gcr.io/${GOOGLE_PROJECT_ID}/${API_IMAGE}:$TRAVIS_COMMIT ./backend;
-    docker image tag gcr.io/${GOOGLE_PROJECT_ID}/${NGINX_IMAGE}:$TRAVIS_COMMIT gcr.io/${GOOGLE_PROJECT_ID}/${NGINX_IMAGE}:latest;
-    docker image tag gcr.io/${GOOGLE_PROJECT_ID}/${API_IMAGE}:$TRAVIS_COMMIT gcr.io/${GOOGLE_PROJECT_ID}/${API_IMAGE}:latest;
-    docker push gcr.io/${GOOGLE_PROJECT_ID}/${NGINX_IMAGE};
-    docker push gcr.io/${GOOGLE_PROJECT_ID}/${API_IMAGE};
+    # =================================== Release ===================================
+    sudo cp .env.prod .env
+
+    docker image build -t $RELEASE_API:$TRAVIS_COMMIT ./backend;
+    docker image build -t $RELEASE_NGINX:$TRAVIS_COMMIT -f nginx/k8s.Dockerfile .;
+    docker image tag $RELEASE_API:$TRAVIS_COMMIT $RELEASE_API:latest;
+    docker image tag $RELEASE_NGINX:$TRAVIS_COMMIT $RELEASE_NGINX:latest;
+    docker push $RELEASE_NGINX;
+    docker push $RELEASE_API;
+
+    # =================================== Stage =====================================
+    sudo cp .env.stage .env
+
+    docker image build -t $STAGE_API:$TRAVIS_COMMIT ./backend;
+    docker image build -t $STAGE_NGINX:$TRAVIS_COMMIT -f nginx/k8s.Dockerfile .;
+    docker image tag $STAGE_API:$TRAVIS_COMMIT $STAGE_API:latest;
+    docker image tag $STAGE_NGINX:$TRAVIS_COMMIT $STAGE_NGINX:latest;
+    docker push $STAGE_NGINX;
+    docker push $STAGE_API;
+
+    # =================================== GKE =======================================
 
     # Access cluster
     gcloud --quiet config set project $GOOGLE_PROJECT_ID
@@ -22,22 +43,26 @@ if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; th
     gcloud --quiet config set compute/zone $ZONE
     gcloud --quiet container clusters get-credentials $CLUSTER
 
-    # Rolling update
-    kubectl set image deployment/${NGINX_DEPLOYMENT} ${NGINX_CONTAINER}=gcr.io/${GOOGLE_PROJECT_ID}/${NGINX_IMAGE}:$TRAVIS_COMMIT;
-    kubectl set image deployment/${API_DEPLOYMENT} ${API_CONTAINER}=gcr.io/${GOOGLE_PROJECT_ID}/${API_IMAGE}:$TRAVIS_COMMIT;
+    # Release deployment update
+    kubectl set image deployment/${NGINX_DEPLOYMENT} ${NGINX_CONTAINER}=$RELEASE_NGINX:$TRAVIS_COMMIT;
+    kubectl set image deployment/${API_DEPLOYMENT} ${API_CONTAINER}=$RELEASE_API:$TRAVIS_COMMIT;
 
-    # Remove secret and deploy to docker hub for public use
+    # Stage deployment update
+    kubectl set image deployment/${NGINX_DEPLOYMENT} --namespace=stage ${NGINX_CONTAINER}=$STAGE_NGINX:$TRAVIS_COMMIT;
+    kubectl set image deployment/${API_DEPLOYMENT} --namespace=stage ${API_CONTAINER}=$STAGE_API:$TRAVIS_COMMIT;
+
+    # =================================== Public ====================================
     sudo cp .env.example .env
     sudo cp /dev/null gs_credential.json
     sudo cp /dev/null backend/gs_credential.json
 
-    # Rebuild and push to docker hub
+    # Rebuild both and push to docker hub
     docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
-    docker image build -t ${DOCKER_ORG}/${NGINX_IMAGE}:$TRAVIS_COMMIT -f nginx/k8s.Dockerfile .;
-    docker image build -t ${DOCKER_ORG}/${API_IMAGE}:$TRAVIS_COMMIT ./backend;
-    docker image tag ${DOCKER_ORG}/${NGINX_IMAGE}:$TRAVIS_COMMIT ${DOCKER_ORG}/${NGINX_IMAGE}:latest;
-    docker image tag ${DOCKER_ORG}/${API_IMAGE}:$TRAVIS_COMMIT ${DOCKER_ORG}/${API_IMAGE}:latest;
-    docker push ${DOCKER_ORG}/${NGINX_IMAGE};
-    docker push ${DOCKER_ORG}/${API_IMAGE};
+    docker image build -t $PUBLIC_API:$TRAVIS_COMMIT ./backend;
+    docker image build -t $PUBLIC_NGINX:$TRAVIS_COMMIT -f nginx/k8s.Dockerfile .;
+    docker image tag $PUBLIC_API:$TRAVIS_COMMIT $PUBLIC_API:latest;
+    docker image tag $PUBLIC_NGINX:$TRAVIS_COMMIT $PUBLIC_NGINX:latest;
+    docker push $PUBLIC_NGINX;
+    docker push $PUBLIC_API;
 
 fi
