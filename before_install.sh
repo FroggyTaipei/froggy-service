c67cda475f28b7c5a9c9cb4bde0070662dc19f33
travishen.tw <travishen.tw@gmail.com>
2019-02-19 21:20:21 +0800

    Deploy dispatching

--- before_install.sh
+++ before_install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ];
+if { [ "$TRAVIS_BRANCH" == "master" ] ||
+     [ "$TRAVIS_BRANCH" == "staging" ] ||
+     [ "$TRAVIS_BRANCH" == "release" ]; } && [ "$TRAVIS_PULL_REQUEST" == "false" ];
 then
   openssl aes-256-cbc -K $encrypted_1fa28bd21641_key -iv $encrypted_1fa28bd21641_iv -in secrets.tar.enc -out secrets.tar -d
   tar xvf secrets.tar
