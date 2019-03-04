773d4353514d3f757103b9733d5f39538cd2a403
travishen.tw <travishen.tw@gmail.com>
2019-03-05 00:53:51 +0800

    Add schema on vuetale api

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('', admin.site.urls, name='admin'),
     path('api/', include(api.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('api/docs/', schema_view),
+    path('api/swagger/', schema_view),
     path('api/csrftoken/', get_token),
 ]
 
