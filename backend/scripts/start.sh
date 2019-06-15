64bfe5f57775c3511b3897a416c35278c6c91a56
travishen <travishen.tw@gmail.com>
2019-06-15 18:03:39 +0800

    Use command initing superuser

--- backend/scripts/start.sh
+++ backend/scripts/start.sh
@@ -11,6 +11,7 @@ python manage.py collectstatic --noinput --verbosity 0
 
 python manage.py loaddata region type site sendgrid-template.yaml
 python manage.py loaddata cases.test.yaml arranges.test.yaml
-echo "from apps.users.tests.setup import create_superuser; create_superuser()" | python manage.py shell
+
+python manage.py init_superuser test@test.test 123456
 
 python manage.py runserver_plus 0.0.0.0:8000
