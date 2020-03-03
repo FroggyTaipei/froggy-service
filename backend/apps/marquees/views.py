f02bd95f3232b48ca370c4d4b4779b6ddfcaa7f5
travishen <travishen.tw@gmail.com>
2020-03-03 22:27:06 +0800

    enhancement(api): Cache enum like API

--- backend/apps/marquees/views.py
+++ backend/apps/marquees/views.py
@@ -1,3 +1,6 @@
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from rest_framework.permissions import AllowAny
 from .serializers import MarqueeMessageSerializer
@@ -9,3 +12,7 @@ class MarqueeMessageViewSet(ReadOnlyModelViewSet):
     serializer_class = MarqueeMessageSerializer
     permission_classes = [AllowAny]
     http_method_names = ['get']
+
+    @method_decorator(cache_page(60 * 5))
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
